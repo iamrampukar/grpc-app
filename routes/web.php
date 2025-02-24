e32b671596d3ac06dbc21c3aeec289f9a7c484e9
Ram Pukar <rampukar_mi@yonefu.info>
2025-02-24 15:37:44 +0545

    GRPC: v1 module managed.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 /** @var \Laravel\Lumen\Routing\Router $router */
-
+use App\Proto\Person;
+use App\Proto\People;
+use App\Proto\BaseModel;
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -15,4 +17,62 @@
 
 $router->get('/', function () use ($router) {
     return $router->app->version();
+});
+
+$router->get('/proto', function () use ($router) {
+    $p = new Person();
+    $p->setName('Ram Pukar');
+    return response($p->serializeToJsonString())
+    ->header('content-type','application/json');
+});
+
+
+$router->get('/proto-people', function () use ($router) {
+    $people = new People();
+    $people->setPeople([
+        (function() {
+            $p = new Person();
+            $p->setName('Ram Pukar');
+            return $p;
+        })(),
+        (function() {
+            $p = new Person();
+            $p->setName('Adwika');
+            return $p;
+        })()
+    ]);
+
+    
+    return response($people->serializeToJsonString())
+    ->header('content-type','application/json');
+});
+
+$router->get('/proto-people-v1', function () use ($router) {
+    $people = new People();
+    $people->setPeople([
+        (function() {
+            $p = new Person();
+            $p->setBaseModel((function(){
+                $bm = new BaseModel();
+                $bm->setId(1);
+                return $bm;
+            })());
+            $p->setName('Ram Pukar v1');
+            return $p;
+        })(),
+        (function() {
+            $p = new Person();
+            $p->setBaseModel((function(){
+                $bm = new BaseModel();
+                $bm->setId(2);
+                return $bm;
+            })());
+            $p->setName('Ram Pukar v2');
+            return $p;
+        })(),
+    ]);
+
+    
+    return response($people->serializeToJsonString())
+    ->header('content-type','application/json');
 });
